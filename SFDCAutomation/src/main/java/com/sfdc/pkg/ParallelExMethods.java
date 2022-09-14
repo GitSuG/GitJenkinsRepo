16f6266df18c41ff30ba87b49780f9cbba70e884
Sujata Ghorpade <ghorpade@Sujatas-MacBook-Air.local>
2022-09-14 13:26:43 -0700

    SFDC project upload

---
+++ SFDCAutomation/src/main/java/com/sfdc/pkg/ParallelExMethods.java
@@ -0,0 +1,51 @@
+package com.sfdc.pkg;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ParallelExMethods {
+
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	
+	@Test
+	public void m1() {
+		System.out.println("m1");
+	}
+	
+	@Test
+	public void m2() {
+		System.out.println("m2");
+	}
+	
+	@Test
+	public void m3() {
+		System.out.println("m3");
+	}
+	
+	@Test
+	public void m4() {
+		System.out.println("m4");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
+}
